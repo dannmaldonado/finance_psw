113c08f8af89bb9a30292c183d22862bfc7eb116
Danilo Maldonado <dannsanto@gmail.com>
2023-07-04 00:20:33 -0300

    first commit

---
+++ perfil/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Categoria(models.Model):
+    categoria = models.CharField(max_length=50)
+    essencial = models.BooleanField(default=False)
+    valor_planejamento = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.categoria
